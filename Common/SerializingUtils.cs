3655e6de0afda01029e15de02701144e586f156f
Trivaxy <itsmeomar2003@yahoo.com>
2020-07-07 20:09:16 +0300

    Use BinaryReaders and BinaryWriters instead of byte arrays, plus actual progress

--- Common/SerializingUtils.cs
+++ Common/SerializingUtils.cs
@@ -1,40 +1,92 @@
 ﻿using Mono.Cecil;
+using System;
 using System.IO;
 
-namespace Common
+namespace Terraweave.Common
 {
+	// TODO: make all forms of type serialization accommodate generics and more access modifiers
 	public static class SerializingUtils
 	{
-		// TODO: make this accommodate for other stuff
-		public static byte[] SerializeFieldDefinition(FieldDefinition field)
-		{
-			using (MemoryStream stream = new MemoryStream())
-			{
-				using (BinaryWriter writer = new BinaryWriter(stream))
-				{
-					writer.Write(field.Name);
-					writer.Write(field.IsStatic);
-					writer.Write(field.IsPublic);
-					writer.Write(SerializeTypeReference(field.FieldType));
+		public static ModuleDefinition TerrariaModule;
 
-					return stream.ToArray();
-				}
-			}
+		public static void SerializeTypeDefinition(TypeDefinition type, BinaryWriter writer)
+		{
+			writer.Write(type.FullName);
+			writer.Write(type.IsPublic);
+			writer.Write(type.Fields.Count);
+
+			foreach (FieldDefinition field in type.Fields)
+				SerializeFieldDefinition(field, writer);
 		}
 
-		// TODO: make this accommodate for other stuff
-		public static byte[] SerializeTypeReference(TypeReference type)
+		public static TypeDefinition DeserializeTypeDefinition(BinaryReader reader)
 		{
-			using (MemoryStream stream = new MemoryStream())
-			{
-				using (BinaryWriter writer = new BinaryWriter(stream))
-				{
-					writer.Write(type.FullName);
-					writer.Write(type.IsArray);
-				}
+			string typeName = reader.ReadString();
+			string @namespace = typeName.Split('.')[0];
 
-				return stream.ToArray();
+			TypeAttributes attributes = reader.ReadBoolean() ? TypeAttributes.Public : TypeAttributes.NotPublic;
+
+			TypeDefinition type = new TypeDefinition(
+				@namespace,
+				typeName.Substring(@namespace.Length + 1),
+				attributes
+				);
+
+			int fieldCount = reader.ReadInt32();
+
+			for (int i = 0; i < fieldCount; i++)
+			{
+				FieldDefinition field = DeserializeFieldDefinition(reader);
+				type.Fields.Add(field);
 			}
+
+			return type;
+		}
+
+		public static void SerializeFieldDefinition(FieldDefinition field, BinaryWriter writer)
+		{
+			writer.Write(field.Name);
+			writer.Write(field.IsPublic);
+			writer.Write(field.IsStatic);
+			SerializeTypeReference(field.FieldType, writer);
+		}
+
+		public static FieldDefinition DeserializeFieldDefinition(BinaryReader reader)
+		{
+			string fieldName = reader.ReadString();
+
+			FieldAttributes attributes = reader.ReadBoolean() ? FieldAttributes.Public : FieldAttributes.Private;
+
+			if (reader.ReadBoolean())
+				attributes |= FieldAttributes.Static;
+
+			FieldDefinition field = new FieldDefinition(
+				fieldName,
+				attributes,
+				DeserializeTypeReference(reader)
+				);
+
+			return field;
+		}
+
+		public static void SerializeTypeReference(TypeReference type, BinaryWriter writer)
+		{
+			writer.Write(type.FullName);
+		}
+
+		public static TypeReference DeserializeTypeReference(BinaryReader reader)
+		{
+			string typeName = reader.ReadString();
+			string @namespace = typeName.Split('.')[0];
+
+			TypeReference type = new TypeReference(
+				@namespace,
+				typeName.Substring(@namespace.Length + 1),
+				TerrariaModule,
+				TerrariaModule
+				);
+
+			return type;
 		}
 	}
 }
--- Differ/Patching/PatchCreator.cs
+++ Differ/Patching/PatchCreator.cs
@@ -1,7 +1,9 @@
 ﻿using Mono.Cecil;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Terraweave.Common;
 using Terraweave.Common.Patching;
 
 namespace Terraweave.Differ.Patching
@@ -16,28 +18,107 @@ namespace Terraweave.Differ.Patching
 			if (!File.Exists(moddedAssemblyPath))
 				Program.Panic("Could not find the modded Terraria.exe");
 
-			ModuleDefinition terraria = ModuleDefinition.ReadModule(terrariaAssemblyPath);
-			ModuleDefinition moddedTerraria = ModuleDefinition.ReadModule(moddedAssemblyPath);
+			ReaderParameters parameters = new ReaderParameters() { AssemblyResolver = GetAssemblyResolver() };
+
+			ModuleDefinition terraria = ModuleDefinition.ReadModule(terrariaAssemblyPath, parameters);
+			ModuleDefinition moddedTerraria = ModuleDefinition.ReadModule(moddedAssemblyPath, parameters);
+
+			SerializingUtils.TerrariaModule = terraria;
+
+			int patchCount = 0;
 
 			TypeDefinition[] injectedTypes = moddedTerraria.GetTypes()
 				.Where(type => terraria.GetType(type.FullName) == null)
 				.ToArray();
 
+			patchCount += injectedTypes.Count();
+
 			using (MemoryStream stream = new MemoryStream())
 			{
 				using (BinaryWriter writer = new BinaryWriter(stream))
 				{
-					writer.Write("TWEAVE");
+					writer.Write(System.Text.Encoding.ASCII.GetBytes("TWEAVE"));
+
+					writer.Write(patchCount);
 
 					foreach (TypeDefinition type in injectedTypes)
 					{
 						Log($"Detected type to inject: {type.FullName}");
-						writer.Write(new TypeInjectPatch(type).SerializePatch());
+						writer.Write(PatchTypes.TypeInject);
+						new TypeInjectPatch(type).SerializePatch(writer);
 					}
 				}
 
 				File.WriteAllBytes("patch.tweave", stream.ToArray());
 			}
+
+			TestDeserializePatches();
+		}
+
+		public static IAssemblyResolver GetAssemblyResolver()
+		{
+			DefaultAssemblyResolver resolver = new DefaultAssemblyResolver();
+
+			if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+			{
+				string[] xnaDirectories = new string[]
+				{
+				"Microsoft.Xna.Framework",
+				"Microsoft.Xna.Framework.Game",
+				"Microsoft.Xna.Framework.Graphics",
+				"Microsoft.Xna.Framework.Xact"
+				};
+
+				foreach (string xnaDirectory in xnaDirectories)
+				{
+					string finalDirectory = Directory.EnumerateDirectories(
+						Environment.ExpandEnvironmentVariables(
+							Path.Combine(
+							"%WINDIR%",
+							"Microsoft.NET",
+							"assembly",
+							"GAC_32",
+							xnaDirectory)
+							))
+							.First();
+
+					resolver.AddSearchDirectory(finalDirectory);
+				}
+			}
+
+			return resolver;
+		}
+
+		// this will be moved to installer eventually, just here for testing
+		public static void TestDeserializePatches()
+		{
+			Patch[] patches;
+
+			using (BinaryReader reader = new BinaryReader(File.Open("patch.tweave", FileMode.Open)))
+			{
+				if (System.Text.Encoding.ASCII.GetString(reader.ReadBytes(6)) != "TWEAVE")
+					Program.Panic("Not a tweave file!");
+
+				int patchCount = reader.ReadInt32();
+				patches = new Patch[patchCount];
+
+				for (int i = 0; i < patchCount; i++)
+				{
+					byte patchType = reader.ReadByte();
+
+					switch (patchType)
+					{
+						case PatchTypes.TypeInject:
+							patches[i] = new TypeInjectPatch(reader);
+							break;
+					}
+				}
+			}
+
+			foreach (Patch patch in patches)
+				patch.Apply(SerializingUtils.TerrariaModule);
+
+			SerializingUtils.TerrariaModule.Write("PatchedTerraria.exe");
 		}
 
 		public static void Log(string message) => Console.WriteLine(message);
