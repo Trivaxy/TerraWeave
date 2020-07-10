063f7b270b7e850e5d1ccd5be56d67e6d23ce64f
Trivaxy <itsmeomar2003@yahoo.com>
2020-07-10 20:16:29 +0300

    add support for nested types

---
+++ Common/Patching/NestedTypeInjectPatch.cs
@@ -0,0 +1,37 @@
+﻿using Mono.Cecil;
+using System.IO;
+
+namespace Terraweave.Common.Patching
+{
+	public class NestedTypeInjectPatch : Patch
+	{
+		public TypeReference ParentType;
+		public TypeDefinition NestedType;
+
+		public NestedTypeInjectPatch(BinaryReader reader) => DeserializePatch(reader);
+
+		public NestedTypeInjectPatch(TypeReference parentType, TypeDefinition nestedType)
+		{
+			ParentType = parentType;
+			NestedType = nestedType;
+		}
+
+		public override void SerializePatch(BinaryWriter writer)
+		{
+			SerializingUtils.SerializeTypeReference(ParentType, writer);
+			SerializingUtils.SerializeTypeDefinition(NestedType, writer);
+		}
+
+		public override Patch DeserializePatch(BinaryReader reader)
+		{
+			ParentType = SerializingUtils.DeserializeTypeReference(reader);
+			NestedType = SerializingUtils.DeserializeTypeDefinition(reader);
+			return this;
+		}
+
+		public override void Apply(ModuleDefinition terraria)
+		{
+			terraria.GetType(ParentType.FullName).NestedTypes.Add(NestedType);
+		}
+	}
+}
--- Common/Patching/PatchTypes.cs
+++ Common/Patching/PatchTypes.cs
@@ -5,5 +5,6 @@ namespace Terraweave.Common.Patching
 	public static class PatchTypes
 	{
 		public const byte TypeInject = 0;
+		public const byte NestedTypeInject = 1;
 	}
 }
