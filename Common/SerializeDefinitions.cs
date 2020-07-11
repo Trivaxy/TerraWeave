f2354b93f8ccd360b8130bf617474ce90b18e25a
Trivaxy <itsmeomar2003@yahoo.com>
2020-07-12 02:08:06 +0300

    Ignore serialization of constants as it is not needed

--- Common/SerializeDefinitions.cs
+++ Common/SerializeDefinitions.cs
@@ -40,9 +40,11 @@ namespace Terraweave.Common
 					SerializeGenericParameter(generic, writer);
 			}
 
-			writer.Write(type.Fields.Count);
+			FieldDefinition[] fields = type.Fields.Where(field => !field.HasConstant).ToArray();
 
-			foreach (FieldDefinition field in type.Fields)
+			writer.Write(fields.Length);
+
+			foreach (FieldDefinition field in fields)
 				SerializeFieldDefinition(field, writer);
 
 			writer.Write(type.Methods.Count);
@@ -225,172 +227,6 @@ namespace Terraweave.Common
 				);
 
 			return property;
-		}
-
-		public static void SerializeInstruction(Instruction instruction, BinaryWriter writer)
-		{
-			Code opCode = instruction.OpCode.Code;
-			OperandType operandType = instruction.OpCode.OperandType;
-			object operand = instruction.Operand;
-
-			writer.Write((int)opCode);
-			writer.Write((int)operandType);
-
-			switch (operandType)
-			{
-				case OperandType.InlineType:
-					SerializeTypeReference(operand as TypeReference, writer);
-					break;
-
-				case OperandType.InlineMethod:
-					SerializeMethodReference(operand as MethodReference, writer);
-					break;
-
-				case OperandType.InlineField:
-					SerializeFieldReference(operand as FieldReference, writer);
-					break;
-			}
-
-			if (operandType == OperandType.InlineString)
-				writer.Write(operand as string);
-			else if (operandType == OperandType.ShortInlineI)
-			{
-				if (opCode == Code.Ldc_I4_S)
-					writer.Write((sbyte)operand);
-				else
-					writer.Write((byte)operand);
-			}
-			else if (operandType == OperandType.InlineI)
-				writer.Write((int)operand);
-			else if (operandType == OperandType.InlineI8)
-				writer.Write((long)operand);
-			else if (operandType == OperandType.ShortInlineR)
-				writer.Write((float)operand);
-			else if (operandType == OperandType.InlineR)
-				writer.Write((double)operand);
-			else if (opCode == Code.Calli)
-				SerializeCallSite(operand as CallSite, writer);
-			else if (operandType == OperandType.InlineVar || operandType == OperandType.ShortInlineVar)
-				SerializeVariableDefinition(operand as VariableDefinition, writer);
-			else if (operandType == OperandType.InlineTok)
-				SerializeMetadataTokenProvider(operand as IMetadataTokenProvider, writer);
-			else if (operandType == OperandType.InlineArg || operandType == OperandType.ShortInlineArg)
-				SerializeParameterDefinition(operand as ParameterDefinition, writer);
-			else if (operandType == OperandType.InlineBrTarget || operandType == OperandType.ShortInlineBrTarget)
-				SerializeInstruction(operand as Instruction, writer);
-			else if (operandType == OperandType.InlineSwitch)
-			{
-				Instruction[] instructions = operand as Instruction[];
-				writer.Write(instructions.Length);
-
-				foreach (Instruction instr in instructions)
-					SerializeInstruction(instr, writer);
-			}
-		}
-
-		public static Instruction DeserializeInstruction(BinaryReader reader)
-		{
-			Code opCode = (Code)reader.ReadInt32();
-			OperandType operandType = (OperandType)reader.ReadInt32();
-			object operand = null;
-
-			#region Instruction stuff
-			switch (operandType)
-			{
-				case OperandType.InlineType:
-					operand = DeserializeTypeReference(reader);
-					break;
-
-				case OperandType.InlineMethod:
-					operand = DeserializeMethodReference(reader);
-					break;
-
-				case OperandType.InlineField:
-					operand = DeserializeFieldReference(reader);
-					break;
-			}
-
-			if (operandType == OperandType.InlineString)
-				operand = reader.ReadString();
-			else if (operandType == OperandType.ShortInlineI)
-			{
-				if (opCode == Code.Ldc_I4_S)
-					operand = reader.ReadSByte();
-				else
-					operand = reader.ReadByte();
-			}
-			else if (operandType == OperandType.InlineI)
-				operand = reader.ReadInt32();
-			else if (operandType == OperandType.InlineI8)
-				operand = reader.ReadInt64();
-			else if (operandType == OperandType.ShortInlineR)
-				operand = reader.ReadSingle();
-			else if (operandType == OperandType.InlineR)
-				operand = reader.ReadDouble();
-			else if (opCode == Code.Calli)
-				operand = DeserializeCallSite(reader);
-			else if (operandType == OperandType.InlineVar || operandType == OperandType.ShortInlineVar)
-				operand = DeserializeVariableDefinition(reader);
-			else if (operandType == OperandType.InlineArg || operandType == OperandType.ShortInlineArg)
-				operand = DeserializeParameterDefinition(reader);
-			else if (operandType == OperandType.InlineBrTarget || operandType == OperandType.ShortInlineBrTarget)
-				operand = DeserializeInstruction(reader);
-			else if (operandType == OperandType.InlineTok)
-				operand = DeserializeMetadataTokenProvider(reader);
-			else if (operandType == OperandType.InlineSwitch)
-			{
-				Instruction[] instructions = new Instruction[reader.ReadInt32()];
-
-				for (int i = 0; i < instructions.Length; i++)
-					instructions[i] = DeserializeInstruction(reader);
-
-				operand = instructions;
-			}
-
-			OpCode finalOpCode = CodeToOpCode[opCode];
-
-			if (operand == null)
-				return Instruction.Create(finalOpCode);
-
-			Type operandObjType = operand.GetType();
-
-			if (operandObjType == typeof(TypeReference))
-				return Instruction.Create(finalOpCode, operand as TypeReference);
-			else if (operandObjType == typeof(MethodReference))
-				return Instruction.Create(finalOpCode, operand as MethodReference);
-			else if (operandObjType == typeof(FieldReference))
-				return Instruction.Create(finalOpCode, operand as FieldReference);
-			else if (operandObjType == typeof(string))
-				return Instruction.Create(finalOpCode, operand as string);
-			else if (operandObjType == typeof(sbyte))
-				return Instruction.Create(finalOpCode, (sbyte)operand);
-			else if (operandObjType == typeof(byte))
-				return Instruction.Create(finalOpCode, (byte)operand);
-			else if (operandObjType == typeof(int))
-				return Instruction.Create(finalOpCode, (int)operand);
-			else if (operandObjType == typeof(long))
-				return Instruction.Create(finalOpCode, (long)operand);
-			else if (operandObjType == typeof(float))
-				return Instruction.Create(finalOpCode, (float)operand);
-			else if (operandObjType == typeof(double))
-				return Instruction.Create(finalOpCode, (double)operand);
-			else if (operandObjType == typeof(CallSite))
-				return Instruction.Create(finalOpCode, operand as CallSite);
-			else if (operandObjType == typeof(VariableDefinition))
-				return Instruction.Create(finalOpCode, operand as VariableDefinition);
-			else if (operandObjType == typeof(ParameterDefinition))
-				return Instruction.Create(finalOpCode, operand as ParameterDefinition);
-			else if (operand is IMetadataTokenProvider)
-			{
-				return Instruction.Create(finalOpCode, operand as FieldDefinition);
-			}
-			else if (operandObjType == typeof(Instruction))
-				return Instruction.Create(finalOpCode, operand as Instruction);
-			else if (operandObjType == typeof(Instruction[]))
-				return Instruction.Create(finalOpCode, operand as Instruction[]);
-			#endregion
-
-			throw new Exception("Failed to deserialize instruction: this should never happen");
 		}
 
 		public static void SerializeCallSite(CallSite callSite, BinaryWriter writer)
